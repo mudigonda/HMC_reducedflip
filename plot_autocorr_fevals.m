d7a71087084adab28ff86329119d33434b25fed1
Mayur Mudigonda <mayur@computer.org>
2013-02-11 20:52:48 -0800

    modified plot_fevals to fix r,g,b,y

---
+++ plot_autocorr_fevals.m
@@ -0,0 +1,12 @@
+%%Plots the auto-correlation of the samples from the various models. It
+%%uses a sliding window scale. If the sampler works well for a set of
+%%parameters, the autocorrelation reaches zero. This plot though compares
+%%with x-axis on function evaluations just to make things easier.
+%%Input = Xstandard, Xstandard_persist, Xreduced_flip, X_2mom
+%%Output = handle to plot
+function h_ac = plot_autocorr_fevals(fevalsstandard,fevalsstandard_persist,fevalsreduced_flip,...
+    fevals_2mom, acstd, acstd_per, ac_red, ac_2mom)
+
+
+
+end
