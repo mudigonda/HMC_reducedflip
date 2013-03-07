ce3ba528a536dced236809534898d5c49a26a166
Jascha Sohl-Dickstein <jascha.sohldickstein@gmail.com>
2013-03-06 19:00:41 -0800

    use cell arrays for the different cases, rather than spaghetti code

--- plot_autocorr_samples.m
+++ plot_autocorr_samples.m
@@ -1,18 +1,15 @@
-function [h] = plot_autocorr_samples(Xstandard,Xstandard_persist,...
-    Xreduced_flip,X_2_momentum)
-
-ac_std=calc_autocorr(Xstandard);
-ac_std_per=calc_autocorr(Xstandard_persist);
-ac_red_flip=calc_autocorr(Xreduced_flip);
-ac_2mom=calc_autocorr(X_2_momentum);
+function [h] = plot_autocorr_samples(X, names)
 
 h=figure(222);
-plot(ac_std,'r');
-hold on;
-plot(ac_std_per,'g');
-plot(ac_red_flip,'b');
-plot(ac_2mom,'y');
-legend('standard','standard, persistent mom','reduced flip','two momentum');
+clf();
+
+for ii in length(X)
+    ac{ii} = calc_autocorr(X{ii})
+    plot(ac{ii});
+    hold on;
+end
+
+legend(names);
 xlabel('Auto correlation windows');
 ylabel('Auto correlation values');
 
--- plot_fevals.m
+++ plot_fevals.m
@@ -2,27 +2,17 @@
 %%function evaluations
 %%Inputs = feval_std,feval_std_persist,feval_redflip,feval_2mom
 %%Outputs = handle to plot
-function h_feval = plot_fevals(fevalsstandard,fevalsstandard_persist,fevalsreduced_flip...
-    ,feval_2_momentum)
+function h_feval = plot_fevals(fevals, names)
 h_feval = figure(111);
-if nargin < 4
-    plot([fevalsstandard(2:end,1),fevalsstandard_persist(2:end,1),'r',...
-    fevalsreduced_flip(2:end,1)],'g',...
-    [fevalsstandard(2:end,2),fevalsstandard_persist(2:end,2),'b',...
-        fevalsreduced_flip(2:end,2)]);
-    legend('standard','standard, persistent mom','reduced flip','two momentum');
-    xlabel('function evaluations');
-    ylabel('percent error diff');
-    title('percent error diff of model and sample covariances - diagonoal elements only');
-else
-    plot(fevalsstandard(2:end,1),fevalsstandard(2:end,2),'r',...
-        fevalsstandard_persist(2:end,1),fevalsstandard_persist(2:end,2),'g',...
-    fevalsreduced_flip(2:end,1),fevalsreduced_flip(2:end,2),'b',...
-    feval_2_momentum(2:end,1),feval_2_momentum(2:end,2),'y');       
-    legend('standard','standard, persistent mom','reduced flip','two momentum');
-    xlabel('function evaluations');
-    ylabel('percent error diff');
-    title('percent error diff of model and sample covariances - diagonoal elements only');
+
+clf();
+
+for ii in length(X)
+    plot(fevals{ii}(2:end,1), fevals{ii}(2:end,2));
+    hold on;
 end
 
-end
+legend(names);
+xlabel('function evaluations');
+ylabel('percent error diff');
+title('percent error diff of model and sample covariances - diagonoal elements only');
