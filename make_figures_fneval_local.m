10b81bcb1a31993618d07dccc711f469fe5e2ad8
Mayur Mudigonda <mayur@computer.org>
2013-05-22 19:15:16 -0700

    saved file names go to HOME directory so it's platform independent

--- make_figures_fneval_local.m
+++ make_figures_fneval_local.m
@@ -1,5 +1,6 @@
 function make_figures_fneval_local(LeapSize,epsilon,beta)
 
+HOME=getenv('HOME');
 whos
 clc;
 close all;
@@ -37,22 +38,19 @@ end
 FEVAL_MAX = 5000000
 % modelname='2dCircle100'
 modelname='100dMOG'
+Nsamp = 1000;
+opts_init.BatchSize = 10;
+opts_init.DataSize = 100;
 savestr = strcat('ModelName-',modelname,'-LeapSize-',int2str(opts_init.LeapSize),...
     '-epsilon-',int2str(opts_init.epsilon*10),'-Beta-',int2str(opts_init.beta*100)...
-    ,'-fevals-',int2str(FEVAL_MAX));
-% % savestr = strcat('ModelName-',modelname,'-LeapSize-',int2str(opts_init.LeapSize),...
-% %     '-epsilon-',int2str(opts_init.epsilon*10),...
-% %     '-fevals-',int2str(FEVAL_MAX));
-savepath = strcat('/Users/mudigonda/Data/HMC_reducedflip/2d/',savestr);
-figpath1 = strcat('/Users/mudigonda/Data/HMC_reducedflip/2d/figures/',savestr,'autocor');
-figpath2 = strcat('/Users/mudigonda/Data/HMC_reducedflip/2d/figures/',savestr,'fneval');
-% number of times to call the sampler
-Nsamp = 1000;
-% number of sampling stpdf to take in each sampler call
-% 			opts_init.T = 1;
-opts_init.BatchSize = 10;
-% number of data dimensions
-opts_init.DataSize = 100;
+    ,'-fevals-',int2str(FEVAL_MAX),'-Nsamp-',int2str(Nsamp)...
+    ,'-BS-',int2str(opts_init.BatchSize),'-DS-',int2str(opts_init.DataSize));
+
+savepath = strcat(HOME,'/Data/HMC_reducedflip/2d/',savestr);
+figpath1 = strcat(HOME,'/Data/HMC_reducedflip/2d/figures/',savestr,'autocor');
+figpath2 = strcat(HOME,'/Data/HMC_reducedflip/2d/figures/',savestr,'autocor-fevals');
+
+
 opts_init.funcevals = 0;
 
 % scaling factor for energy function
@@ -61,7 +59,7 @@ opts_init.funcevals = 0;
 for ii=1:opts_init.DataSize
    rng(ii);
    J{ii}=diag(exp(linspace(log(1e-6), log(1), opts_init.DataSize)).*rand(1,opts_init.DataSize));
-   Mu{ii}=randn(opts_init.DataSize,1)*ceil(10e-4*rand(1));
+   Mu{ii}=randn(opts_init.DataSize,1)*ceil(10e4*rand(1));
 end
 
 
@@ -144,13 +142,13 @@ ii=1;
             for jj=1:length(names)
                 avg_fevals{jj}=fevals{jj}(end,1)/size(X{jj},3);
             end
-            h1=plot_autocorr_samples(X, names,avg_fevals);
+            [h1,h2]=plot_autocorr_samples(X, names,avg_fevals);
 						disp('Autocorr plot completed')
 %             h2=plot_fevals(fevals, names);
 						disp('Fevals plot completed')
             disp(savestr)
             saveas(h1,figpath1,'pdf');
-%             saveas(h2,figpath2,'pdf');
+            saveas(h2,figpath2,'pdf');
             save(savepath);
         end
         ii = ii + 1;
