5947223d47fabf3b97efd05c2905defa4c8db437
Jascha Sohl-Dickstein <jascha.sohldickstein@gmail.com>
2014-01-08 17:12:58 -0800

    better plot settings

--- plot_autocorr_samples.m
+++ plot_autocorr_samples.m
@@ -2,13 +2,12 @@ function [h_scaled,h_notscaled,ac] = plot_autocorr_samples(X, names,avg_fevals, 
 
 fig_shape = [1,1,4,3];
 
-colorlist=['r','g','b','k','m','y'];
+colorlist={'r:','b--','m-.','k-','y-','r:'};
 h_notscaled=figure(222);
 clf();
 set(h_notscaled, 'Units','inches');
 set(h_notscaled, 'Position', fig_shape);
 set(h_notscaled, 'Color', 'w');
-grid on;
 
 num_steps = 500;
 max_shift = min([max_shift, size(X{1},3)]);
@@ -20,10 +19,11 @@ target_shifts = unique(target_shifts);
 for ii=1:length(X)
     ac{ii} = calc_autocorr(X{ii},target_shifts);
     ac{ii} = ac{ii}/ac{ii}(1);
-    plot(target_shifts,ac{ii},colorlist(ii));
+    plot(target_shifts,ac{ii},colorlist{ii});
     hold on;
 end
 
+grid on;
 legend(names);
 xlabel('Sampling Steps');
 ylabel('Autocorrelation');
@@ -33,16 +33,16 @@ clf();
 set(h_scaled, 'Units','inches');
 set(h_scaled, 'Position', fig_shape);
 set(h_scaled, 'Color', 'w');
-grid on;
 
 min_fevals = inf;
 
 for ii=1:length(X)
     min_fevals = min([max(target_shifts*avg_fevals{ii}), min_fevals]);
-    plot(target_shifts*avg_fevals{ii},ac{ii},colorlist(ii));
+    plot(target_shifts*avg_fevals{ii},ac{ii},colorlist{ii});
     hold on;
 end
 axis([0, min_fevals, -0.1, 1.01]);
+grid on;
 legend(names);
 xlabel('Gradient Evaluations');
 ylabel('Autocorrelation');
